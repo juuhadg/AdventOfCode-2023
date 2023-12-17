6308969d5a5d5385c8773751c9b42aec1c6e3107
juuhadg <juku.jj@gmail.com>
2023-12-17 12:27:32 -0300

    Update day-15-part2.js

--- advent-of-code-2023/day-15/day-15-part2.js
+++ advent-of-code-2023/day-15/day-15-part2.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-const input = fs.readFileSync('./example.txt').toString().trim().split(',')
+const input = fs.readFileSync('./input.txt').toString().trim().split(',')
     var lensMap = {}
 for(var i = 0;i< input.length;i++) {
 
@@ -9,7 +9,7 @@ for(var i = 0;i< input.length;i++) {
     let string = input[i]
     let len = string.replace(/[=-]/g, " ")
     let valorAtual = 0;
-    var label = string.split(/[=-]/)[0]
+    var label = string.split(/[=-]/)[0].trim()
         for(var char of label) {
             let asciiCode = char.charCodeAt(0)
             valorAtual += asciiCode
@@ -22,22 +22,25 @@ for(var i = 0;i< input.length;i++) {
 
         if(string.includes('=')) {
             let focalLength = Number(string.split('=')[1])
-            if(!lensMap[valorAtual].some(arr=>arr[0].includes(label))) {
-
-                var labelEValor = [label,focalLength]
+            var index = lensMap[valorAtual].findIndex(arr => arr.includes(label));
+            var labelEValor = [label,focalLength]
+            if(index === -1) {
                 lensMap[valorAtual].push(labelEValor)
+               
 
             } else {
-                var index = lensMap[valorAtual].findIndex(arr => arr[0].includes(label));
                 lensMap[valorAtual][index] = labelEValor
+                
             }
 
 
         } else if(string.includes('-')) {
-                var index = lensMap[valorAtual].findIndex(arr => arr[0].includes(label));
+                var index = lensMap[valorAtual].findIndex(arr => arr.includes(label));
+                
+                
                 if(index != -1) {
-
-                     lensMap[valorAtual] = lensMap[valorAtual].splice(index,1)
+                    
+                    lensMap[valorAtual].splice(index, 1);
                 }
         
 
@@ -56,8 +59,6 @@ for(var chave in lensMap) {
             
             for(var j = 0; j < lensMap[chave].length;j++) {
                 let valorDaLente = (Number(chave) + 1) * (j+1) * lensMap[chave][j][1];
-                console.log((Number(chave) + 1) * (j+1) * lensMap[chave][j][1])
-            
                 
                 soma += valorDaLente
             }
@@ -66,4 +67,3 @@ for(var chave in lensMap) {
 
 
 console.log(soma)
-console.log(lensMap)
